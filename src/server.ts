1857c6114f2b0888c2716ff51b0caaa0d04510fd
mr-valentos <nerpavlad@gmail.com>
2023-02-27 15:10:04 +0200

    Init Express App with typescript

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from 'cors';
+
+const PORT = 8000;
+
+const users = [
+    { id: 1, name: 'Joe Biden', carColorId: 5 },
+    { id: 2, name: 'Elon Musk', carColorId: 4 },
+    { id: 3, name: 'Pan Roman', carColorId: 2 },
+];
+
+const app = express();
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send(users);
+})
+
+app.listen(PORT, () => {
+    console.log(`API is ready on http://localhost:${PORT} 🚀🚀🚀`);
+})
