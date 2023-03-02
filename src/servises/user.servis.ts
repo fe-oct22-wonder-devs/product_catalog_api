7467619a172fb781895075c5d76fba51ad0fde9c
mr-valentos <nerpavlad@gmail.com>
2023-03-02 13:42:21 +0200

    get length & get one phone

--- src/servises/user.servis.ts
+++ src/servises/user.servis.ts
@@ -2,7 +2,7 @@ import fs from 'fs/promises';
 import { SortOptions } from '../types/SortOptions';
 import { Phone } from '../types/Phone';
 
-export const getPhones = async(
+export const findMany = async(
   perPageNum: number,
   pageNum: number,
   sort: string,
@@ -36,4 +36,18 @@ export const getPhones = async(
   }
 
   return phones;
+};
+
+export const checkLength = async() => {
+  const phonesJSON = await fs.readFile('./src/data/phones.json', 'utf-8');
+  const phones: Phone[] = JSON.parse(phonesJSON);
+
+  return phones.length;
+};
+
+export const findOne = async(id: string) => {
+  const phonesJSON = await fs.readFile('./src/data/phones.json', 'utf-8');
+  const phones: Phone[] = JSON.parse(phonesJSON);
+
+  return phones.find(phone => phone.id === id);
 };
